051a5be0bd6acdab99e620c2cdaa23d4413362fc
Paweł J. Wal <p@paweljw.al>
2020-10-30 12:02:34 +0100

    More text fancification

--- locale.h
+++ locale.h
@@ -6,9 +6,9 @@
 #define S_PRESS_TO_START "  focus, buddy"
 #define S_POMODORO "  Pomodoro "
 #define S_PAUSED "  -- paused --"
-#define S_DONE "Done! "
-#define S_BREAK_TIME "Break time!"
-#define S_FOCUS_TIME "Time to focus!"
+#define S_DONE "   Done! "
+#define S_BREAK_TIME "  Break time!"
+#define S_FOCUS_TIME " Time to focus!"
 #define S_SEP "/"
 #define S_SEC_PRE "     "
 
